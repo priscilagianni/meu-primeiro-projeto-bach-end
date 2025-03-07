fd11a52e4dd9a9fd95be4f1fe709ebac46aa5429
priscilagianni <priscila_gianni@hotmail.com>
2025-03-07 11:32:20 -0300

    Descrição breve das mudanças

--- mulheres.js
+++ mulheres.js
@@ -1,30 +1,30 @@
 const express = require("express") 
-const router = express.Router()//configuramos a rota
+const router = express.Router()
 
 const app = express()
 const porta = 3333
 
 
-const mulheres = [ // criamos nossa lista de melheres
-{
-    nome:'Priscila Gianni',
-    imagem:'Imagem do WhatsApp de 2025-01-23 à(s) 21.14.37_0017ddfb.jpg',
-    minibio:'estudante'
-},
-{
-    nome:'Maria Lucia',
-    imagem:'Imagem do WhatsApp de 2025-01-23 à(s) 21.14.37_0017ddfb.jpg',
-    minibio:'Mainha'
-},
-{
-    nome: 'Livia Celi',
-    imagem:'Imagem do WhatsApp de 2025-01-23 à(s) 21.14.37_0017ddfb.jpg',
-    minibio:'irmã'
-}
-]//aqui eu tenho uma lista de mulheres que é um array de objetos
+const mulheres = [ 
+   {
+    nome: 'Simara Conceição',
+    imagem: 'https://bit.ly/3LJIyOF',
+    minibio: 'Desenvolvedora e instrutora',
+    }, 
+    {
+    nome: 'Iana Chan',
+    imagem: 'https://bit.ly/3JCXBqP',
+    minibio: 'CEO & Founder da PrograMaria',
+    },
+    {
+    nome: 'Luana Pimentel',
+    imagem: 'https://bit.ly/3FKpFaz',
+    minibio: 'Senior Staff Software Engineer',
+    }     
+    ]
     
-function mostraMulheres(request, response){ //criamos a função mulheres
-response.json(mulheres)//resposta nossa lista de mulheres
+function mostraMulheres(request, response){ 
+response.json(mulheres)
 }
 
 function mostraPorta(){
--- ola.js
+++ ola.js
@@ -1,20 +1,20 @@
-//repetir a estrutura do server
 
 
-const express = require("express") //Router esta vindo dentro do express
+
+const express = require("express") 
 const router = express.Router()
 const app = express()
 const porta = 3333
 
-function mostraOla(request, response) { // que faça requisção e envie uma resposta quando for chamada
-response.send("Olá, mundo!")//nao quer mostrar no console, quero que mostre no navegador exemplo. localhost:3333/ola, quero que apareça lá a função send envia/manda
+function mostraOla(request, response) { 
+response.send("Olá, mundo!")
 }
 
 
 function mostraPorta(){
 console.log("Servidor criado e rodando na porta ",  porta)
 }
-//criar o endereço barra ola e dizer que quando acessar o endereço execute a função mostra ola
-app.use(router.get('/ola', mostraOla))//get metodo que paga informações, o get espera duas informação la dentro, 1 ele espera qual o endereço? /ola e depois separa com virgula mostraOla
+
+app.use(router.get('/ola', mostraOla))
 
 app.listen(porta, mostraPorta)
